a5af6ab5a04059112a7c608e114f8f176d45aa59
Aditya YV <adityayv802@gmail.com>
2025-03-02 01:00:01 -0600

    main() method update

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include "const.h"
 #include "state_machine.h"
+#include "UART_intf.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,12 +11,13 @@ void setup() {
   Serial.begin(115200);
   
   initStateMachine();
+  initUART();
 
   Serial.printf("Initial State: %d\n", getStateMachineState());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500);
-  Serial.printf("Current State: %d\n", getStateMachineState()); 
+  stateMachine(); 
+  writeUART();
 }
